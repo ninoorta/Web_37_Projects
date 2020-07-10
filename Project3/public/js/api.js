498bb48ae3f73127d95804dd4eb97a185cec2520
Ninoorta <blackcathp9x@gmail.com>
2020-07-11 01:02:01 +0700

    update signin everytime

--- Project3/public/js/api.js
+++ Project3/public/js/api.js
@@ -1,5 +1,6 @@
 const baseUrl = '/api'
 const productApiUrl = `${baseUrl}/product`
+const signInApiUrl = `/api/auth/sign-in`
 
 
 
@@ -8,11 +9,27 @@ async function createProduct(count, pageSize, currentIndex, e, sortOption) {
         title: document.getElementById('title').value
     }
 
-    let accessToken = `
-    eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZWUwZjY4MTUxNDk0MDFkYjhiMWMzNDkiLCJlbWFpbCI6ImJsYWNrY2F0aHA5eEBnbWFpbC5jb20iLCJuYW1lIjoiVHVhbiIsInN0YXRlIjoiYXZhaWxhYmxlIiwiX192IjowLCJpYXQiOjE1OTM1OTYwMDgsImV4cCI6MTU5MzYxNzYwOH0.GvuyFPaSNjyCXVpZmIR6yPK1frVZHXwlMT9mrBz5C2Q
-    `
 
-    let res = await fetch(productApiUrl, {
+    // --------------------Get access token every time---------------------
+    let signIn = {
+        "email": "blackcathp9x@gmail.com",
+        "password": "123456"
+    }
+
+    let res = await fetch(signInApiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(signIn)
+    })
+
+    let result = await res.json();
+    let accessToken = result.accessToken;
+    //  ---------------------------------------------------------------------
+
+
+    res = await fetch(productApiUrl, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -35,16 +52,31 @@ async function updateProduct(count, pageSize, currentIndex, e, sortOption) {
     let chosenID = document.getElementsByClassName('modal-footer-update')[0].attributes.id.value
 
     console.log(chosenID);
-    
+
     let data = {
         _id: chosenID,
         title: document.getElementById('title-update').value
     }
 
-    let accessToken = `
-    eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZWUwZjY4MTUxNDk0MDFkYjhiMWMzNDkiLCJlbWFpbCI6ImJsYWNrY2F0aHA5eEBnbWFpbC5jb20iLCJuYW1lIjoiVHVhbiIsInN0YXRlIjoiYXZhaWxhYmxlIiwiX192IjowLCJpYXQiOjE1OTM1OTYwMDgsImV4cCI6MTU5MzYxNzYwOH0.GvuyFPaSNjyCXVpZmIR6yPK1frVZHXwlMT9mrBz5C2Q
-    `
-    let res = await fetch(productApiUrl, {
+    // --------------------Get access token every time---------------------
+    let signIn = {
+        "email": "blackcathp9x@gmail.com",
+        "password": "123456"
+    }
+
+    let res = await fetch(signInApiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(signIn)
+    })
+
+    let result = await res.json();
+    let accessToken = result.accessToken;
+    //  ---------------------------------------------------------------------
+
+    res = await fetch(productApiUrl, {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json',
@@ -69,10 +101,26 @@ async function updateProduct(count, pageSize, currentIndex, e, sortOption) {
 async function deleteProduct(count, pageSize, currentIndex, e, sortOption) {
 
     let id = document.getElementsByClassName('modal-footer-delete')[0].attributes.id.value;
-    let accessToken = `
-    eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZWUwZjY4MTUxNDk0MDFkYjhiMWMzNDkiLCJlbWFpbCI6ImJsYWNrY2F0aHA5eEBnbWFpbC5jb20iLCJuYW1lIjoiVHVhbiIsInN0YXRlIjoiYXZhaWxhYmxlIiwiX192IjowLCJpYXQiOjE1OTM1OTYwMDgsImV4cCI6MTU5MzYxNzYwOH0.GvuyFPaSNjyCXVpZmIR6yPK1frVZHXwlMT9mrBz5C2Q
-    `
-    let res = await fetch(`${productApiUrl}/${id}`, {
+
+    // --------------------Get access token every time---------------------
+    let signIn = {
+        "email": "blackcathp9x@gmail.com",
+        "password": "123456"
+    }
+
+    let res = await fetch(signInApiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(signIn)
+    })
+
+    let result = await res.json();
+    let accessToken = result.accessToken;
+    //  ---------------------------------------------------------------------
+
+    res = await fetch(`${productApiUrl}/${id}`, {
         method: 'DELETE',
         headers: {
             'Content-Type': 'application/json',
